16a7c0b95016e52729c8a24779298246a896a3fb
Teo230 <teeassassin230@gmail.com>
2022-02-28 15:32:34 +0100

    add edit profile

--- projects/lavoro-io/src/app/pages/settings/settings.component.ts
+++ projects/lavoro-io/src/app/pages/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { SystemService } from '../../services/system.service';
 import { UserService } from '../../services/user.service';
 
@@ -9,12 +10,33 @@ import { UserService } from '../../services/user.service';
 })
 export class SettingsComponent implements OnInit {
 
+  userSub: any;
+
+  profileForm = new FormGroup({
+    username: new FormControl(''),
+    name: new FormControl(''),
+    surname: new FormControl(''),
+    email: new FormControl('')
+  });
+
   constructor(private userService: UserService,
               private systemService: SystemService) {
 
   }
 
   ngOnInit(): void {
+    this.events();
+  }
+
+  private events() {
+    this.userSub = this.systemService.currentUser.subscribe((user)=>{
+      this.profileForm.controls['username'].patchValue(user.username);
+      this.profileForm.controls['name'].patchValue(user.name);
+      this.profileForm.controls['surname'].patchValue(user.surname);
+      this.profileForm.controls['email'].patchValue(user.email);
+
+      this.profileForm.markAsPristine();
+    });
   }
 
 }
